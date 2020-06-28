eb037526a12ba5ab1f82acd0049e05e350f42431
William Cunha <williamoliveira2@hotmail.com>
2020-06-28 12:56:35 -0300

    Fix template test

--- src/Template/Template.spec.tsx
+++ src/Template/Template.spec.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import Template from './Template';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<Template />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const renderWithRouter = (component: JSX.Element) =>
+  render(<MemoryRouter>{component}</MemoryRouter>);
+
+describe('<Template />', () => {
+  it('should renders with logo', () => {
+    const { getByText } = renderWithRouter(<Template />);
+    const logoElement = getByText('logo.svg');
+    expect(logoElement).toBeInTheDocument();
+  });
+
+  it('should renders with footer', () => {
+    const { getByText } = renderWithRouter(<Template />);
+    const footerEement = getByText('© 2020');
+    expect(footerEement).toBeInTheDocument();
+  });
 });
